b17f25075a300133356cf89d4386076e53de6420
drissnafii <drissnafi3@gmail.com>
2025-01-08 10:29:04 +0100

    Récupérer et afficher les commentaires existants dans un format structuré.

--- Commentaires.php
+++ Commentaires.php
@@ -1,12 +1,49 @@
-<?php
-require __DIR__ . '/submit_feedback.php';
+<?php 
+    require __DIR__ . '/config/conn.php';
 
-    try {
-        $sql = "SELECT nom, message, horodatage
-                FROM commentaires";
+    $stmt = $pdo->prepare("SELECT nom, message, horodatage from commentaires");
+    $stmt->execute();
+    $Comments = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-    } catch (PDOException $e) {
-        error_log("error: " . $e->getMessage());
+
+    if ($Comments > 0) {
+
+        echo "<h4>Search for a name in the input field.</h4>
+
+            <input>";
+
+        echo "<div>";
+        echo "<table>";
+        echo "<tr>
+                <th>Name</th><br>
+                <th>Comment</th>
+                <th>Created At</th>
+                <th>Action...</th>
+            </tr>";
+
+            foreach ($Comments as $Comment) {
+                // echo "<pre>";
+                // var_dump($contact[4]); die();
+                // echo "</pre>";
+
+
+                # code...
+                echo "<tr>
+                <td>$Comment->nom</td>  
+                <td>$Comment->message</td>  
+                <td>$Comment->horodatage</td> 
+                <td>
+                    <form method='post'>
+                        <input type='submit' value='modifier' name='modifier' action=modifier.php >
+                        <input type='submit' value='suprimer' name='suprimer' action=suprimer.php >
+                    </form>
+                </td>
+            </tr>";
+            }
+
+        echo "</table>";
+        echo "</div>";
+    } else {
+        echo "Pas de ligne pour l'afficher...";
     }
+?>
